b81ecd4399fc9d22b805583b724be4b80ba5f28e
C@nantk <128438309+conantkd@users.noreply.github.com>
2023-11-19 22:46:17 -0500

    first commit, task 01 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+function coloringBackground() {
+    if (!intervalId) {
+        intervalId = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor();
+            startButton.setAttribute("disabled","true");
+            console.log(intervalId);
+        }
+            , 1000);
+    }
+}
+
+function stop() {
+    clearInterval(intervalId);
+    intervalId = null;
+    startButton.removeAttribute("disabled");
+}
+
+startButton.addEventListener(
+  'click',coloringBackground
+);
+
+stopButton.addEventListener('click', stop);
